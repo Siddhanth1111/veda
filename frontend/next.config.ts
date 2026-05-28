48c0783d71a833216c7249e0b3814debacca412b
Siddhanth Chauhan <siddhanthchauhan1111mpspn@gmail.com>
2026-05-28 23:11:54 +0530

    link updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: isProd ? 'https://veda-backend-virid.vercel.app' : 'http://localhost:5000',
-    NEXT_PUBLIC_SOCKET_URL: isProd ? 'https://veda-backend-virid.vercel.app' : 'http://localhost:5000',
+    NEXT_PUBLIC_API_URL: isProd ? 'https://veda-4-6723.onrender.com' : 'http://localhost:5000',
+    NEXT_PUBLIC_SOCKET_URL: isProd ? 'https://veda-4-6723.onrender.com' : 'http://localhost:5000',
   }
 };
 
